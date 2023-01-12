9bca95b852dfe273a12f4dc49f52a34c782a55e7
Andrii Krenevych <a.krenevych@samsung.com>
2023-01-12 16:06:43 +0200

    initial commit

---
+++ app/src/main/java/com/kre/shoplist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kre.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kre.shoplist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
